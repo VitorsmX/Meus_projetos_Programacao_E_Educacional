3913f32e0f7c997e619d02e7429d06599b4b01b9
Vitor Sousa Mesquita <vitorsmfff@yahoo.com>
2022-08-24 05:36:38 -0300

    novas funcionalidades
    inserido o sistema que substitui virgula por ponto e transforma em float

--- Aprendizado em Py/python sistema de media/sistema_Notas.py
+++ Aprendizado em Py/python sistema de media/sistema_Notas.py
@@ -1,9 +1,9 @@
 #usa biblioteca de estatisticas do python para a média
 from statistics import mean
 
-#função que calcula a média da nota pela biblioteca
-def calculaNota(Dados):
-    calculaMedia = mean(Dados)
+#função que calcula a média da nota pela biblioteca apartir da lista nova criada com dados Float
+def calculaNota(listaDadosFloat):
+    calculaMedia = mean(listaDadosFloat)
     return calculaMedia
 
 #função que limita os caracteres da variavel aluno e das variáveis de notas e emite um erro se for maior que o máximo permitido
@@ -16,38 +16,42 @@ def limitarCaracteresAluno(Aluno):
         return False
         
 
-print('#'*40 + '\nBem vindo ao sistema institucional da Universidade Federal do Acre\n' + '#'*40)
+print('#'*40 + '\nBem vindo ao sistema institucional da Universidade Federal do Tangamandápio\n' + '#'*40)
 
 '''
-Define as variaveis de alunos, notas das AVs, media e nota minima - em seguida, enquanto o numero de caracteres de aluno for maior que o limite, ele vai continuar pedindo para inserir
+Define as variaveis de alunos, notas das AVs, media e nota minima como Strings - em seguida, enquanto o numero de caracteres de aluno for maior que o limite, ele vai continuar pedindo para inserir
 '''
 Aluno = input('Digite seu nome: ')
 while limitarCaracteresAluno(Aluno) == True:
     Aluno = input('Digite seu nome: ')
     pass
 
-notaAV1 = float(input('Digite sua nota da primeira avaliação: '))
+notaAV1 = input('Digite sua nota da primeira avaliação: ')
 
-notaAV2 = float(input('Digite sua nota da segunda avaliação: '))
+notaAV2 = input('Digite sua nota da segunda avaliação: ')
 
-notaAV3 = float(input('Digite sua nota da terceira avaliação: '))
+notaAV3 = input('Digite sua nota da terceira avaliação: ')
 
-notaAV4 = float(input('Digite sua nota da quarta avaliação: '))
+notaAV4 = input('Digite sua nota da quarta avaliação: ')
 
 #Agrupa as notas na variavel dados para calcular a média apartir dela
 Dados = [notaAV1, notaAV2, notaAV3, notaAV4]
 
+#coloca virgula nas strings da lista Dados que foi solicitada inserção, coloca os dados em uma nova lista que vai ter os valores Float(Decimal)
+Dados[0:5] = notaAV1.replace(',','.'), notaAV2.replace(',','.'), notaAV3.replace(',','.'), notaAV4.replace(',','.')
+listaDadosFloat = []
+for DadosFloat in Dados:
+    listaDadosFloat.append(float(DadosFloat))
 
 #define as variáveis para calcular a média
-mediaNotas = round(calculaNota(Dados),2)
-notaMinima = 6
-notaFinalTotal = round(sum(Dados),2)
-notaFinalSemestre1 = round(sum(Dados[0:2]),2)
-notaFinalSemestre2 = round(sum(Dados[2:4]),2)
+mediaNotas = round(calculaNota(listaDadosFloat),2)
+mediaMinima = 6
+notaFinalTotal = round(sum(listaDadosFloat),2)
+notaFinalSemestre1 = round(sum(listaDadosFloat[0:2]),2)
+notaFinalSemestre2 = round(sum(listaDadosFloat[2:4]),2)
 
-
-#checa se a média é maior ou menor que a nota minima
-if(mediaNotas >= notaMinima):
-    print('Parabéns, você passou, sua media final foi %s \nNOTA MINIMA: %s\nNota Final do Primeiro Semestre: %s\nNota Final do Segundo Semestre: %s\nNota Final Total: %s' %(mediaNotas, notaMinima, notaFinalSemestre1, notaFinalSemestre2, notaFinalTotal))
+#checa se a média é maior ou menor que a nota minima e imprime os resultados
+if(mediaNotas >= mediaMinima):
+    print('\n' + '*'*60 + '\nParabéns %s, você passou, sua media final foi %s \nMEDIA MINIMA: %s\nNota Final do Primeiro Semestre: %s\nNota Final do Segundo Semestre: %s\nNota Final Total: %s' %(Aluno.title(), mediaNotas, mediaMinima, notaFinalSemestre1, notaFinalSemestre2, notaFinalTotal))    
 else:
-    print('Infelizmente você não passou, sua media final foi %s \nNOTA MINIMA %s\nNota Final do Primeiro Semestre: %s\nNota Final do Segundo Semestre: %s\nNota Final Total: %s' %(mediaNotas, notaMinima, notaFinalSemestre1, notaFinalSemestre2, notaFinalTotal))
+    print('\n' + '*'*60 + '\nInfelizmente você não passou %s, sua media final foi %s \nMEDIA MINIMA %s\nNota Final do Primeiro Semestre: %s\nNota Final do Segundo Semestre: %s\nNota Final Total: %s' %(Aluno.title(), mediaNotas, mediaMinima, notaFinalSemestre1, notaFinalSemestre2, notaFinalTotal))
